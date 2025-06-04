9ab76a5cd8d1b2721c7e8b100258fac8899616da
NJOGU <157185184+Nj4191@users.noreply.github.com>
2025-06-04 16:24:31 +0300

    1
    ad

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ db.version(1).stores({
     items: '++id,name,Quantity,price'
 });
 
-const ItemForm = document.getElementById('itemsform');
+const ItemForm = document.getElementById('ItemsForm');
 const itemsDiv = document.getElementById('itemsDiv');
 const totalpriceDiv = document.getElementById('totalpricediv');
+
+ ItemForm.onsubmit = async (e) => {
+    e.preventDefault() }
